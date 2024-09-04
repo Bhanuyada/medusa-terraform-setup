490870f80eb85f85a3d4ece4e4759cd42ce6e7be
Bhanuyada <123895390+Bhanuyada@users.noreply.github.com>
2024-09-04 15:42:41 +0530

    Create ecs.tf

---
+++ ecs.tf
@@ -0,0 +1,28 @@
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name        = "medusa-container"
+      image       = "medusajs/medusa:latest" # Replace with your Medusa image
+      cpu         = 256
+      memory      = 512
+      essential   = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
