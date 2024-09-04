921aa0d35ddd8403d1c50ddaf97d755e8df9f8b8
Bhanuyada <123895390+Bhanuyada@users.noreply.github.com>
2024-09-04 15:43:26 +0530

    Create ecs_service.tf

---
+++ ecs_service.tf
@@ -0,0 +1,13 @@
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.medusa_subnet[*].id
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+}
